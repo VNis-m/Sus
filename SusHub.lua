be4c666d6850e2516146c23b763eabf1cfc75895
VNis-m <lephuocquang2k8@gmail.com>
2024-12-31 12:36:09 +0700

    Create SusHub.lua

---
+++ SusHub.lua
@@ -0,0 +1,7 @@
+
+Config = {
+    namescript = "Sus Hub",--Tên Script
+    logoscript = "132159415763089",--ID Logo Script
+    tacgia = "DinoSukuna"-- Tên Tác Giả
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
